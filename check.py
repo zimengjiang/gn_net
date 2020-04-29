fix computing feature gradient, add bilinear interpolation, fix negative sampling

check.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import numpy as np  
 
 def check_batch_slice():
@@ -23,8 +24,17 @@ def check_batch_slice():
     f_idx_2d = torch.from_numpy(f_idx_2d).type(torch.LongTensor)
     f_ext = torch.index_select(f_2d, -1, f_idx_2d).transpose(0,1)
     tmp
-    
+
+def check_sobel():
+    batch_size = 10
+    channels = 3
+    h, w = 24, 24
+    x = torch.randn(batch_size, channels, h, w)
+    conv = nn.Conv2d(1, 1, 4, 2, 1)
+    output = conv(x.view(-1, 1, h, w)).view(batch_size, channels, h//2, w//2)
+    print(output.shape)
 
 
 if __name__ == "__main__":
-    check_batch_slice()
+    # check_batch_slice()
+    check_sobel()

cmu_dataset.py
@@ -2,7 +2,7 @@ import numpy as np
 from PIL import Image
 from pathlib import Path
 from glob import glob 
-from corres_sampler import random_select_positive_matches, random_select_negative_matches
+from corres_sampler import random_select_positive_matches
 import scipy.io
 import h5py # for loading v7.3 .mat 
 
@@ -102,7 +102,7 @@ class CMUDataset(Dataset):
     '''
     def default_transform(self):
         return transforms.Compose([
-            # transforms.Resize((192, 256)), # check image dim, resize (H, W)?  just for fast debugging 
+            transforms.Resize((192, 256)), # check image dim, resize (H, W)?  just for fast debugging 
             transforms.ToTensor(),
             transforms.Normalize(
                 mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),

network/gn_loss.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn 
 import numpy as np
-from utils import MyHardNegativePairSelector
+from utils import MyHardNegativePairSelector, bilinear_interpolation, batched_eye_like, torch_gradient
 
+
+# TODO: resize the image and corresponding matches
 
 class GNLoss(nn.Module):
     '''
@@ -13,38 +15,38 @@ class GNLoss(nn.Module):
         self.margin = margin
         self.pair_selector = MyHardNegativePairSelector()
         self.lamda = lamda
-    
-    def batched_eye_like(self, x, n):
-        """Create a batch of identity matrices.
-        Args:
-            x: a reference torch.Tensor whose batch dimension will be copied.
-            n: the size of each identity matrix.
-        Returns:
-            A torch.Tensor of size (B, n, n), with same dtype and device as x.
-        """
-        return torch.eye(n).to(x)[None].repeat(len(x), 1, 1)
 
     def extract_features(self, f, indices):
         '''
         f: BxCxHxW
         indicies: BxNx2
         '''
+        B,C,H,W = f.shape
         N = indices.shape[1]
-        b, c, h, w = f.shape
-        f_permuted = f.permute(1,0,2,3)
-        f_2d = f_permuted.reshape((c, b*h*w))
-        f_idx_2d = np.zeros((b*N))
-        # modified
-        f_idx_2d_cuda_tensor = torch.zeros((b*N)).cuda()
-        for b_th in range(b):
-            m = indices[b_th]
-            # f_idx_2d[b_th*(N):(b_th+1)*N] = b_th*w*h + m[:,1]*w + m[:,0]
-            # modified
-            f_idx_2d_cuda_tensor[b_th*(N):(b_th+1)*N] = b_th*w*h + m[:,1]*w + m[:,0]
-            f_idx_2d = f_idx_2d_cuda_tensor.cpu().numpy()
-        f_idx_2d = torch.floor(torch.from_numpy(f_idx_2d)).type(torch.LongTensor).to(f_2d.device)
+        for b in range(f.shape[0]):
+            f_bth = bilinear_interpolation(f[b,:,:,:], indices[b, :,:])
+            if not b:
+                f_2d = f_bth
+            else:
+                f_2d = torch.cat((f_2d, f_bth), dim = 1)
+        return f_2d.transpose(0,1)
 
-        return torch.index_select(f_2d, -1, f_idx_2d).transpose(0,1)
+        # N = indices.shape[1]
+        # b, c, h, w = f.shape
+        # f_permuted = f.permute(1,0,2,3)
+        # f_2d = f_permuted.reshape((c, b*h*w))
+        # f_idx_2d = np.zeros((b*N)) # for cpu debugging
+        # # modified
+        # # f_idx_2d_cuda_tensor = torch.zeros((b*N)).cuda()
+        # for b_th in range(b):
+        #     m = indices[b_th]
+        #     f_idx_2d[b_th*(N):(b_th+1)*N] = b_th*w*h + m[:,1]*w + m[:,0]
+        #     # modified
+        #     # f_idx_2d_cuda_tensor[b_th*(N):(b_th+1)*N] = b_th*w*h + m[:,1]*w + m[:,0]
+        #     # f_idx_2d = f_idx_2d_cuda_tensor.cpu().numpy()
+        # f_idx_2d = torch.floor(torch.from_numpy(f_idx_2d)).type(torch.LongTensor).to(f_2d.device)
+
+        # return torch.index_select(f_2d, -1, f_idx_2d).transpose(0,1)
         # return torch.index_select(f_2d, -1, f_idx_2d)
 
     def compute_contrastive_loss(self, fa, fb, N, pos):
@@ -66,10 +68,11 @@ class GNLoss(nn.Module):
         # compute start point and its feature
         # ub = corres_pos['b']
         B,N,_ = ub.shape
-        xs = torch.round(torch.rand(ub.shape) + ub)
-        p_x = torch.clamp(xs[:,:,0], max = (self.max_size_x-1) // level, min = 0)
-        p_y = torch.clamp(xs[:,:,1], max = (self.max_size_y-1) // level, min = 0)
-        xs = torch.stack((p_x, p_y), dim = -1)
+        # xs = torch.round(torch.rand(ub.shape) + ub) #round this causes bugs in bilinear interpolation!!! all weights becomes 0!
+        
+        # p_x = torch.clamp(xs[:,:,0], max = (self.max_size_x-1) // level, min = 0)
+        # p_y = torch.clamp(xs[:,:,1], max = (self.max_size_y-1) // level, min = 0)
+        # xs = torch.stack((p_x, p_y), dim = -1)
         # tmp,_ = torch.max(xs[:,:,0],dim=-1)
         # tmp,_ = torch.max(xs[:,:,1], dim = -1)
         # xs = torch.round(torch.rand(ub.shape) + ub)
@@ -77,26 +80,30 @@ class GNLoss(nn.Module):
             
         # xs = torch.round(torch.rand(ub.shape) + ub) # start at most 1 pixel away from u_b
 
+        xs = torch.rand(ub.shape) + ub
         # torch.clamp(min=0, max = self.max_size[1], xs[:]) # self.max_size: H x W
         f_s = self.extract_features(fb, xs)
         # compute residual
         r = f_s - f_t
         # compute Jacobian
-        '''
-        TODO: np.gradient
-        '''
-        f_s_gy, f_s_gx = np.gradient(fb.cpu().detach().numpy(), axis=(2, 3)) # numerical derivative: J = d(fb)/d(xs), feature gradients # to check if it is correct
+
         # modified
-        J_xs_x = self.extract_features(torch.from_numpy(f_s_gx), xs).cuda()
-        J_xs_y = self.extract_features(torch.from_numpy(f_s_gy), xs).cuda()
-        J = torch.stack([J_xs_x, J_xs_y], dim=-1) # todo: check dim
+        f_s_gx, f_s_gy = torch_gradient(fb) # problematic 
+        J_xs_x = self.extract_features(f_s_gx, xs)
+        J_xs_y = self.extract_features(f_s_gy, xs)
+
+        # f_s_gy, f_s_gx = np.gradient(fb.cpu().detach().numpy(), axis=(2, 3)) # numerical derivative: J = d(fb)/d(xs), feature gradients # to check if it is correct
+        # J_xs_x = self.extract_features(torch.from_numpy(f_s_gx), xs)#.cuda()
+        # J_xs_y = self.extract_features(torch.from_numpy(f_s_gy), xs)#.cuda()
+        
+        J = torch.stack([J_xs_x, J_xs_y], dim=-1).type(torch.float32) # todo: check dim
         # compute Heissian
         eps = 1e-9 # for invertibility, need to be smaller
-        H = J.transpose(1,2) @ J + eps * self.batched_eye_like(J, J.shape[2])
-        r = r.reshape((r.shape[0],r.shape[1],1))
+        H = (J.transpose(1,2) @ J + eps * batched_eye_like(J, J.shape[2]))
+        r = r.reshape((r.shape[0],r.shape[1],1)).type(torch.float32)
         b = J.transpose(1,2) @ r
         xs_reshape = xs.reshape(B*N,2,1)
-        miu = xs_reshape - torch.inverse(H) @ b
+        miu = xs_reshape - torch.inverse(H) @ b.type(torch.float32)
         # first error term
         e1 = 0.5 * ((ub.reshape(B*N,2,1) - miu).transpose(1,2)).type(torch.float32) @ H @ (ub.reshape(B*N,2,1) - miu).type(torch.float32) # check dim, very unsure
         e1 = torch.sum(e1)
@@ -123,12 +130,12 @@ class GNLoss(nn.Module):
         self.max_size_y = F_a[-1].shape[2]
         '''get neg pairs, do it only for the finest feature'''
          # slice features for positive matches
-        fa_4_sliced = self.extract_features(F_a[-1], known_matches['a']) # //4 if the input image is scaled
+        fa_4_sliced = self.extract_features(F_a[-1], known_matches['a']/4) # //4 if the input image is scaled
         # fa_4_sliced_reshape = fa_4_sliced.reshape((B,N,C))
-        fb_4_sliced = self.extract_features(F_b[-1], known_matches['b']) # //4
+        fb_4_sliced = self.extract_features(F_b[-1], known_matches['b']/4) # //4
         # fb_4_sliced_reshape = fb_4_sliced.reshape((B,N,C))
         # get hard negative samples
-        negative_matches = self.pair_selector.get_pairs(fa_4_sliced, fb_4_sliced, known_matches.cuda()) #//4 inside pair selector
+        negative_matches = self.pair_selector.get_pairs(fa_4_sliced, fb_4_sliced, known_matches) #//.cuda 4 inside pair selector
 
         '''compute loss for each scale'''
         loss= 0
@@ -139,10 +146,10 @@ class GNLoss(nn.Module):
                 fa_sliced_pos = fa_4_sliced
                 fb_sliced_pos = fb_4_sliced
             else:
-                fa_sliced_pos = self.extract_features(F_a[i], known_matches['a'] // (level)) #(level*4)) #TODO: use bilinear interpolation in extract_features
-                fb_sliced_pos = self.extract_features(F_b[i], known_matches['b'] // (level)) #(level*4))
-            fa_sliced_neg = self.extract_features(F_a[i], negative_matches['a'] // level) # don't //4 here. negative_matches are in the same scale as known_matches//4
-            fb_sliced_neg = self.extract_features(F_b[i], negative_matches['b'] // level)
+                fa_sliced_pos = self.extract_features(F_a[i], known_matches['a'] / (level*4)) #(level*4)) #TODO: use bilinear interpolation in extract_features
+                fb_sliced_pos = self.extract_features(F_b[i], known_matches['b'] / (level*4)) #(level*4))
+            fa_sliced_neg = self.extract_features(F_a[i], negative_matches['a'] / level) # don't //4 here. negative_matches are in the same scale as known_matches//4
+            fb_sliced_neg = self.extract_features(F_b[i], negative_matches['b'] / level)
 
             '''compute contrastive loss'''
             # loss of positive pairs:
@@ -151,7 +158,7 @@ class GNLoss(nn.Module):
             loss_neg = self.compute_contrastive_loss(fa_sliced_neg, fb_sliced_neg, N, pos = False)
 
             '''compute gn loss'''
-            loss_gn = self.compute_gn_loss(fa_sliced_pos, F_b[i], known_matches['b'] // (level), (level)) #//4, //no 4 for the second level
-            loss = (loss_pos + loss_neg) + self.lamda * loss_gn + loss
+            loss_gn = self.compute_gn_loss(fa_sliced_pos, F_b[i], known_matches['b'] / (level*4), (level)) #//4, //no 4 for the second level
+            loss = (loss_pos + loss_neg) + (self.lamda * loss_gn) + loss  
 
         return loss

run.py
@@ -10,9 +10,9 @@ import torch.optim as optim
 '''set up data loaders'''
 
 # todo: make it configurable 
-trainset = CMUDataset(root = '/cluster/work/riner/users/PLR-2020/lechen/gn_net/gn_net_data',
+trainset = CMUDataset(root = '/Users/zimengjiang/code/3dv/public_data',
                       name = 'cmu',
-                      image_folder = 'cmu_extended',
+                      image_folder = 'images',
                       pair_info_folder = 'correspondence_data',
                       cmu_slice_all = True,
                       cmu_slice = 6,
@@ -39,7 +39,7 @@ model = GNNet(embedding_net)
 model = model.to(device)
 # set up loss 
 margin = 1.
-loss_fn = GNLoss(margin = 1, lamda = 0.005)
+loss_fn = GNLoss(margin = 1, lamda = 0.003)
 lr = 1e-6
 optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay = 1e-3)
 scheduler = optim.lr_scheduler.StepLR(optimizer, 8, gamma=0.1, last_epoch=-1) # optional

utils.py
@@ -2,6 +2,7 @@ from itertools import combinations
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 class PairSelector:
     """
@@ -33,8 +34,8 @@ class MyHardNegativePairSelector(PairSelector):
 # TODO: adjust the weight
         scale_position = 1e-2
         weight = 10 
-        match_pos_in_1 = match_pos['a'] #//4
-        match_pos_in_2 = match_pos['b'] #//4
+        match_pos_in_1 = match_pos['a'] /4 #//4
+        match_pos_in_2 = match_pos['b'] /4 #//4
         B,N,_ = match_pos_in_1.shape
         C = embedding1.shape[1]        
 
@@ -102,5 +103,52 @@ def batched_eye_like (x, n):
         A torch.Tensor of size (B, n, n), with same dtype and device as x.
     """
     return torch.eye(n).to(x)[None].repeat(len(x), 1, 1)
-    
+
+
+def bilinear_interpolation(grid, idx):
+    # grid: C x H x W
+    # idx: N x 2
+    _, H, W = grid.shape
+    x = idx[..., 0]
+    y = idx[..., 1]
+    x0 = torch.clamp(torch.floor(x), 0, W-1)
+    y0 = torch.clamp(torch.floor(y), 0, H-1)
+    x1 = torch.clamp(torch.ceil(x), 0, W-1)
+    y1 = torch.clamp(torch.ceil(y), 0, H-1)
+    weight00 = (x1 - x) * (y1 - y)
+    weight01 = (x1 - x) * (y - y0)
+    weight10 = (x - x0) * (y1 - y)
+    weight11 = (x - x0) * (y - y0)
+    x0 = x0.type(torch.LongTensor)
+    y0 = y0.type(torch.LongTensor)
+    x1 = x1.type(torch.LongTensor)
+    y1 = y1.type(torch.LongTensor)
+    grid00 = grid[..., y0, x0]
+    grid01 = grid[..., y0, x1]
+    grid10 = grid[..., y1, x0]
+    grid11 = grid[..., y1, x1]
+    # print(weight00)
+    # print(weight01)
+    # print(weight10)
+    # print(weight11)
+
+    return weight00*grid00 + weight01*grid01 + weight10*grid10 + weight11*grid11
+
+def torch_gradient(f):
+    # f: BxCxHxW
+    # sobel_y = torch.FloatTensor([[-1., -2., -1.], [0., 0., 0.], [1., 2., 1.]]).view(1,1,3,3).expand(1,f.shape[1],3,3) # .cuda()
+    # sobel_x = torch.FloatTensor([[-1., 0., 1.],[-2., 0., 2.],[-1., 0., 1.]]).view(1,1,3,3).expand(1,f.shape[1],3,3) # .cuda()
+    # f_gradx = F.conv2d(f, sobel_x, stride=1, padding=1)
+    # f_grady = F.conv2d(f, sobel_y, stride=1, padding=1)
+
+    b,c,h,w = f.shape
+    # x = torch.randn(batch_size, channels, h, w)
+    # conv = nn.Conv2d(1, 1, 4, 2, 1)
+    # output = conv(x.view(-1, 1, h, w)).view(batch_size, channels, h//2, w//2)
+    # sobel operator torch.grad = False checked.
+    sobel_y = torch.FloatTensor([[-1., -2., -1.], [0., 0., 0.], [1., 2., 1.]]).view(1,1,3,3) # .cuda()
+    sobel_x = torch.FloatTensor([[-1., 0., 1.],[-2., 0., 2.],[-1., 0., 1.]]).view(1,1,3,3) # pytorch performs cross-correlation instead of convolution in information theory
+    f_gradx = F.conv2d(f.view(-1,1,h,w), sobel_x, stride=1, padding=1).view(b,c,h,w)
+    f_grady = F.conv2d(f.view(-1,1,h,w), sobel_y, stride=1, padding=1).view(b,c,h,w)
+    return f_gradx, f_grady
 