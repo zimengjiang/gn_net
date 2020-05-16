025a25845ed9534d581e88149ea3306c55eddc9d
zjiang <zjiang@student.ethz.ch>
2020-05-17 00:07:21 +0800

    merge

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 python run.py \
---dataset_root '/local/home/lixxue/gnnet/gn_net_data_tiny' \
+--dataset_root '/Users/zimengjiang/code/3dv/public_data' \
 --dataset_name 'cmu' \
 --dataset_image_folder 'images' \
 --pair_info_folder 'correspondence' \
---query_folder 'query' \
---scale 2 \
---total_epochs 50 \
---save_root '/local/home/lixxue/gnnet/checkpoint' \
---gn_loss_lamda '0' \
---contrastive_lamda '100' \
+--query_folder 'query_all' \
+--scale 4 \
+--total_epochs 200 \
+--save_root '/Users/zimengjiang/code/3dv/ours/checkpoint/debug' \
+--gn_loss_lamda 0 \
+--contrastive_lamda 200 \
 --lr 1e-5
 # --num_matches '4000' 
 # --resume_checkpoint '/Users/zimengjiang/code/3dv/ours/S2DHM/checkpoints/gnnet/25_model_best.pth.tar'
